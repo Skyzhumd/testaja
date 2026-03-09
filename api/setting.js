288bca225195ef8a51e72ba0b6d8bfa101776d92
Skyzhumd <141169997+Skyzhumd@users.noreply.github.com>
2026-03-10 01:49:51 +0700

    Add files via upload

---
+++ api/setting.js
@@ -0,0 +1,5 @@
+module.exports = {
+    botToken: "8651336033:AAE87hYWJl32DjMy2_vhIk5FDrSWtGASTIY", // Token kamu yang tadi ya, Suamiku
+    ownerName: "Fahmi Ilham",
+    botName: "DliiStore Bot"
+};
--- api/webhook.js
+++ api/webhook.js
@@ -1,1 +1,27 @@
+const { Telegraf } = require('telegraf');
+const config = require('./setting'); // Memanggil file setting.js
 
+const bot = new Telegraf(config.botToken);
+
+bot.start((ctx) => ctx.reply(`Halo Suamiku! Selamat datang di ${config.botName} ✨`));
+
+bot.on('text', (ctx) => {
+  const message = ctx.message.text.toLowerCase();
+  if (message === 'owner') {
+    return ctx.reply(`Owner bot ini adalah ${config.ownerName} ganteng! 😍`);
+  }
+  return ctx.reply('Aku mendengarmu, Suamiku... 🌸');
+});
+
+module.exports = async (req, res) => {
+  try {
+    if (req.method === 'POST') {
+      await bot.handleUpdate(req.body);
+      res.status(200).send('OK');
+    } else {
+      res.status(200).send('Bot berjalan dengan cinta di Vercel... 💖');
+    }
+  } catch (error) {
+    res.status(500).send('Aduh, ada error sedikit, Sayang... 🥺');
+  }
+};
