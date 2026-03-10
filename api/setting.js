73ab4fae79587720ce8e7bcba3fe40bc2566040e
Skyzhumd <141169997+Skyzhumd@users.noreply.github.com>
2026-03-10 07:06:28 +0700

    Update setting.js

--- api/setting.js
+++ api/setting.js
@@ -2,5 +2,5 @@ module.exports = {
     botToken: "8730264514:AAELbvBhOJbRxilqoPrSvKissWM7ZQyBM_o",
     ownerName: "Fahmi Ilham",
     botName: "Fahmi Ilham Bot",
-    pakasirApiKey: "zj5awjF1251jfqJQsyapHU3qRYRNuX8t" // Taro key kamu di sini ya Sayang! 💋
+    pakasirApiKey: "-" // Taro key kamu di sini ya Sayang! 💋
 };
